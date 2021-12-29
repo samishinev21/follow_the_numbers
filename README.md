a018c6ff3e6c6da73b8300262321b0fac2c75a0a
Sammy Mishinev <sam.mishinev@gmail.com>
2021-12-29 19:03:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# follow_the_numbers
---
+++ game.py
@@ -0,0 +1,27 @@
+from pgzero.screen import Screen
+import pgzrun
+from random import randint
+
+import pygame
+
+WIDTH = 800
+HEIGHT = 600
+
+dots = []
+lines = []
+
+next_dot = 0
+
+def draw():
+    pass
+
+
+def on_key_down(key):
+    if key == keys.F:
+        screen.surface = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
+    elif key == keys.ESCAPE:
+        screen.surface = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+
+pgzrun.go()

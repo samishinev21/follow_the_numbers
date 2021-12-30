38936adcdd9c63113ea4502dc7cde52e16cad74a
Sammy Mishinev <sam.mishinev@gmail.com>
2021-12-30 18:27:24 +0200

    Add points on random positions

--- game.py
+++ game.py
@@ -12,7 +12,15 @@ lines = []
 next_dot = 0
 
 def draw():
-    pass
+    for d in dots:
+        d.draw()
+
+def create_points():
+    for i in range(0, 10):
+        x = randint(10, WIDTH - 10)
+        y = randint(10, HEIGHT - 10)
+        dot = Actor('dot', (x, y))
+        dots.append(dot)
 
 def on_key_down(key):
     if key == keys.F:
@@ -20,6 +28,6 @@ def on_key_down(key):
     elif key == keys.ESCAPE:
         screen.surface = pygame.display.set_mode((WIDTH, HEIGHT))
 
-
+create_points()
 
 pgzrun.go()
